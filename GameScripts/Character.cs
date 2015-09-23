886128a0858533d55f683f5964da40cdefe1c0dd
waterkh <pjclark@email.sc.edu>
2015-09-23 08:30:41 -0400

    Syncing Project
    Adding what I have as of right now. (Mostly just testing)

---
+++ GameScripts/Character.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Character {
+	
+	/*
+	 * 
+	 * Instance Variables
+	 * 
+	 */ 
+	public int health;
+	public int attack;
+	public int speed;
+
+	/*
+	 * 
+	 * Default Constructor
+	 * 
+	 */ 
+	public Character()
+	{
+
+		health = 100;
+		attack = 10;
+		speed = 5;
+
+	}
+
+	/*
+	 * 
+	 * Parameter Constructor
+	 * 
+	 */ 
+	public Character(int aHealthVal, int anAttVal, int aSpeedVal)
+	{
+
+		setHealth(aHealthVal);
+		setAttack(anAttVal);
+		setSpeed(aSpeedVal);
+
+	}
+
+	/*
+	 * 
+	 * Accessors
+	 * 
+	 */ 
+	public void setHealth(int aHealthVal)
+	{
+
+		health = aHealthVal;
+
+	}
+
+	public void setAttack(int anAttVal)
+	{
+
+		attack = anAttVal;
+
+	}
+
+	public void setSpeed(int aSpeedVal)
+	{
+
+		speed = aSpeedVal;
+
+	}
+
+	/*
+	 * 
+	 * Mutators
+	 * 
+	 */ 
+	public int getHealth()
+	{
+
+		return health;
+
+	}
+
+	public int getAttack()
+	{
+
+		return attack;
+
+	}
+
+	public int getSpeed()
+	{
+
+		return speed;
+
+	}
+
+}
---
+++ GameScripts/NanoByte.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class NanoByte : Character {
+		
+	public int experiencePoints;
+
+	/*
+	 *
+	 *	Calling the base class of Character to set the values of health and such
+	 * 
+	**/
+	public NanoByte()
+	{
+
+		setHealth(100);
+		setAttack(10);
+		setSpeed(5);
+		experiencePoints = 0;
+
+	}
+
+	public NanoByte(int aHealth, int anAttack, int aSpeed, int expPts) : base(aHealth, anAttack, aSpeed)
+	{
+
+		setExperience(expPts);
+		
+	}
+
+	public void setExperience(int expPts)
+	{
+
+		experiencePoints = expPts;
+
+	}
+
+	public int getExperience()
+	{
+
+		return experiencePoints;
+
+	}
+
+	/*
+	 * 
+	 * TODO Do we want to increase by a certain value, or just set it to what we want?
+	 * 	Like so: getHealth() + aHealth; OR setHealth(aHealth); OR setHealth(getHealth() + aHealth);
+	 * 
+	 * 
+	**/ 
+	public void levelUpHealth(int aHealth)
+	{
+
+		setHealth(aHealth);
+
+	}
+
+	public void levelUpAttack(int anAttack)
+	{
+		
+		setAttack(anAttack);
+		
+	}
+
+	public void levelUpSpeed(int aSpeed)
+	{
+		
+		setSpeed(aSpeed);
+		
+	}
+
+}
