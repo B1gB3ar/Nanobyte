fb088bfbb221e45fe16ce9aa0a76e01be93b40d7
Will Wells <wellswc@email.sc.edu>
2015-11-02 23:01:32 -0500

    CameraLock and Level 2
    Added a script to lock the camera onto the nanobyte without  instant 180
    degree rotations, and beginnings to Level2

---
+++ GameScripts/CameraLock.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLock : MonoBehaviour {
+	public Transform target;
+	private float distance;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (target.position.x, target.position.y, transform.position.z - distance);
+	}
+}
